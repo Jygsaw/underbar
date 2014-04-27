40ceac8348a568268e5370c254e73cf649d1fac7
Jygsaw <jygsaw@gmail.com>
2014-04-26 18:38:06 -0700

    Implement extend()

--- src/underbar.js
+++ src/underbar.js
@@ -257,6 +257,17 @@ var _ = {};
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var targetObj = obj;
+    if (arguments.length > 1) {
+      // recursively initialize extendObj
+      var extendObj = _.extend.apply(this, Array.prototype.slice.call(arguments, 1));
+
+      // add extendObj properties to targetObj
+      for (var key in extendObj) {
+        targetObj[key] = extendObj[key];
+      }
+    }
+    return targetObj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
